2f4091d701079a1b578b795ce6dc0fd8bcfc0d2e
Aleksey Petukhov <ikazman@yandex.ru>
2021-11-17 16:20:58 +0300

    Added time counter

--- get_summary.py
+++ get_summary.py
@@ -1,5 +1,9 @@
 from text_summarization import text_summarization
+import time
 
 if __name__ == '__main__':
+    start = time.time()
     get_summary = text_summarization.TextSummarization()
     get_summary.summary()
+    end = time.time()
+    print(f'Процесс выполнен за {end - start} сек.')
