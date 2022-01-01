fbba0160a36a2feffee08f5ba7af10e8c374a076
Aleksey Petukhov <ikazman@yandex.ru>
2022-01-01 20:47:27 +0300

    Better pipe. PEP8 screwed

--- text_processing/pipe.py
+++ text_processing/pipe.py
@@ -1,11 +1,17 @@
 import string
+import math
+from collections import defaultdict
 from heapq import nlargest
 
+import pandas as pd
 import nltk
 from nltk import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords as nltk_stopwords
 from nltk.stem import SnowballStemmer
 from pymystem3 import Mystem
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.collocations import QuadgramCollocationFinder
+from nltk.metrics.association import QuadgramAssocMeasures
 
 russian_stemmer = SnowballStemmer('russian')
 
@@ -22,7 +28,9 @@ class Summary:
         self.text = text
         self.processed_text = None
         self.summary_length = 1
-        self.word_frequency = {}
+        self.word_frequency = defaultdict(int)
+        self.word_frequency_by_sentence = defaultdict(int)
+        self.sentences = 0
         self.sent_scores = {}
         self.summary = None
         self.file_name = file
@@ -32,7 +40,7 @@ class Summary:
         берем десятую часть, иначе - одно предолжение"""
         print(f'\nРезюме для текста {self.file_name}.')
         print('Определяем число предложений для резюме.')
-        num_of_sentences = self.text.count('. ')
+        num_of_sentences = len(sent_tokenize(self.text, language='russian'))
         if num_of_sentences > 20:
             self.summary_length = int(round(num_of_sentences / 10, 0))
         elif num_of_sentences > 9:
@@ -58,7 +66,8 @@ class Summary:
         print('Лемматизируем слова, выделяем корень.')
         temp_text = ' '.join(self.processed_text.copy())
         lemmas = self.mystem.lemmatize(temp_text)
-        self.processed_text = [russian_stemmer.stem(lemma) for lemma in lemmas]
+        self.processed_text = [russian_stemmer.stem(lemma) for lemma in lemmas
+                               if lemma != ' ']
 
     def lemmatize_and_stem(self, sentence):
         """Лемматизируем и выделим корень из одного слова."""
@@ -70,10 +79,18 @@ class Summary:
         """Подсчитываем частоту слов в тексте."""
         print('Подсчитываем частоту слов в тексте.')
         for word in self.processed_text:
-            if word not in self.word_frequency:
-                self.word_frequency[word] = 1
-            else:
-                self.word_frequency[word] += 1
+            self.word_frequency[word] += 1
+
+    def count_sentences_with_word(self):
+        """Подсчитываем важность предложений."""
+        print('Подсчитываем обратную частоту.\n')
+        sentence_list = sent_tokenize(self.text, language='russian')
+        self.sentences = len(sentence_list)
+        for sentence in sentence_list:
+            sentence_lem = self.lemmatize_and_stem(sentence.lower())
+            for word in set(word_tokenize(sentence_lem, language='russian')):
+                if word in self.word_frequency.keys():
+                    self.word_frequency_by_sentence[word] += 1
 
     def normilize_word_frequency(self):
         """Нормализуем частоту слов - диапозон от 0 до 1"""
@@ -81,6 +98,15 @@ class Summary:
         max_freq = max(self.word_frequency.values())
         for word in self.word_frequency.keys():
             self.word_frequency[word] = self.word_frequency[word] / max_freq
+
+    def tf_ifd_score(self):
+        for word in self.word_frequency.keys():
+            print('WORD - ', word)
+            print(self.word_frequency[word])
+            print(self.word_frequency_by_sentence[word])
+            if self.word_frequency_by_sentence[word] > 0:
+                self.word_frequency[word] = abs(math.log((self.word_frequency[word] / len(self.processed_text)) * self.word_frequency_by_sentence[word], 10))
+            print(self.word_frequency[word])
 
     def count_sentenses_score(self):
         """Подсчитываем важность предложений."""
@@ -102,6 +128,8 @@ class Summary:
         self.clean_text()
         self.lemmatize_and_stem_text()
         self.count_word_frequency()
+        self.count_sentences_with_word()
+        self.tf_ifd_score()
         self.normilize_word_frequency()
         self.count_sentenses_score()
         self.summary = ' '.join(nlargest(self.summary_length,
